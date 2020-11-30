22b48b8469f018c6eebfb4e1af961beb08eb0436
Mikhail Marychev <v-mimary@microsoft.com>
2020-11-30 19:46:22 +0300

    add the first test

---
+++ Popup.UITest/Tests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using System.Threading;
+
+namespace Popup.UITest
+{
+    [TestFixture(Platform.Android)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("Hello PopopX!"));
+            app.Screenshot("Start PopopX");
+
+            Assert.IsTrue(results.Any());
+
+            Thread.Sleep(2000);
+            app.Screenshot("Start to PRESS");
+            app.Tap(c => c.Marked("I open POPUP for you!"));
+
+            Thread.Sleep(2000);
+            app.Screenshot("Want to see field Name");
+            app.WaitForElement(c => c.Marked("Name:"));
+
+        }
+    }
+}
---
+++ PopupX/PopupX/PopUp.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+
+namespace PopupX
+{
+    public partial class PopUp //: PopupPage
+    {
+        public PopUp()
+        {
+            InitializeComponent();
+        }
+    }
+}
