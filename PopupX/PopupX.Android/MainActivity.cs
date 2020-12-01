c00b3da43b8667907cac485a0c860b14a8578ccb
Mikhail Marychev <v-mimary@microsoft.com>
2020-12-01 16:14:18 +0300

    add appCenter packages

--- PopupX/PopupX.Android/MainActivity.cs
+++ PopupX/PopupX.Android/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace PopupX.Droid
 {
